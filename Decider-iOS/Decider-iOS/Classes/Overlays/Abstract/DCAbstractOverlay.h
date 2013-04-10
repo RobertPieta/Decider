fa8d3720ac9ce6ce02ec17560c67193524893546
Robert P <robert.m.pieta@gmail.com>
2013-04-10 13:01:59 -0500

    added start of overlay imlementation

--- Decider-iOS/Decider-iOS/Classes/Overlays/Abstract/DCAbstractOverlay.h
+++ Decider-iOS/Decider-iOS/Classes/Overlays/Abstract/DCAbstractOverlay.h
@@ -9,5 +9,39 @@
 #import <UIKit/UIKit.h>
 
 @interface DCAbstractOverlay : UIView
+@property(nonatomic, weak, readonly) UIView *view;
+@property(nonatomic, assign, readonly) int x;
+@property(nonatomic, assign, readonly) int y;
+
+@property(nonatomic, assign) float duration;
+@property(nonatomic, strong) NSString *text;
+
+/**
+ * This method creates the overlay view in a parent view at an x and y
+ * @param view - The parent view
+ * @param x - The x coordinate of the upper left hand corner
+ * @param y - The y coordinate of the upper left hand corner
+ * @return A new overlay object
+ */
+-(id)initOverlayInView:(UIView*)view atX:(int)x andY:(int)y;
+
+#pragma mark -
+#pragma mark Display Methods
+
+/**
+ * This method displays the overlay for the defaults duration (2s)
+ */
+-(void)display;
+
+/**
+ * This method displays the overlay for the given duration
+ * @param duration - The duration that the messaage will be on screen
+ */
+-(void)displayForDuration:(float)duration;
+
+/**
+ * This method dismisses the overlay view from the parent view
+ */
+-(void)dismiss;
 
 @end
--- Decider-iOS/Decider-iOS/Classes/Overlays/Abstract/DCAbstractOverlay.m
+++ Decider-iOS/Decider-iOS/Classes/Overlays/Abstract/DCAbstractOverlay.m
@@ -10,4 +10,31 @@
 
 @implementation DCAbstractOverlay
 
+-(id)initOverlayInView:(UIView*)view atX:(int)x andY:(int)y {
+    if(self = [super init]) {
+        _view = view;
+        _x = x;
+        _y = y;
+        
+        _duration = 2.0f;
+        _text = @"";
+    }
+    return self;
+}
+
+#pragma mark -
+#pragma mark Display Methods
+
+-(void)display {
+    
+}
+
+-(void)displayForDuration:(float)duration {
+    
+}
+
+-(void)dismiss {
+    
+}
+
 @end
